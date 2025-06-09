8d6c7a458b38652d2694b4df7540ca05538e6135
Sangamesh MK <sangameshmkuri94@gmail.com>
2025-06-09 13:50:48 +0530

    Create 06_Arithmatic_operators.py

---
+++ Python/06_Arithmatic_operators.py
@@ -0,0 +1,51 @@
+"""
+The common arithmatic operators are :
+    (a) Addition(+)
+    (b) Subtraction(-)
+    (c) Multiplication(*)
+    (d) Division(/)
+    (e) floor division(//)
+    (f) Modulus|Remainder(%)
+    (g) Exponentiation(**)
+    
+    """
+    
+#Ex:01
+a = 20
+b = 6
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)    
+print(a**b)
+
+#O/P:
+26
+14
+120
+3.3333333333333335
+3
+2
+64000000
+
+#Ex:02
+x = 17
+y = 212
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x//y)
+print(x%y)    
+print(x**y)
+
+#O/P: 
+229
+-195
+3604
+0.08018867924528301
+0
+17
+716425989505823792538648540073767626675892576849694732684326913196580735298167964731010124669685975067898374061831880656292000030046430685032024215980154922470642416483879645297157019372574748349503234182198215945149340726204603292122144554876114298662164605761
