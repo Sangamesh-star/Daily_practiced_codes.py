a8e6dc566e3224977d74eb0f74362e16a4a04613
Sangamesh MK <sangameshmkuri94@gmail.com>
2025-06-09 09:15:26 +0530

    Create Assigning_variables.py

---
+++ Assigning_variables.py
@@ -0,0 +1,31 @@
+name = "Sangamesh"
+height = 5.8
+weight = 51
+is_student = True
+
+"""
+Here, •name
+      •height
+      •weight
+      •is_student are the variables.
+And 
+      •"sangamesh" 
+      •5.8 
+      •51
+      •True Are the values.
+which are assigned to the variables.  
+
+"""
+
+
+print(name)
+#O/P: sangamesh
+
+print(height)
+#O/P: 5.8
+
+print(weight)
+#O/P: 51
+
+print(is_student)
+#O/P: True
