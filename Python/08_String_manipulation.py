7ce5b0d91c05d4a33c34b74b2f96808c0f2412d8
Sangamesh MK <sangameshmkuri94@gmail.com>
2025-06-11 09:30:57 +0530

    Create 08_String_manipulation.py

---
+++ Python/08_String_manipulation.py
@@ -0,0 +1,62 @@
+#(★). String manipulation:
+        
+#(1). Common string operations:
+
+#Ex:01 Username Generation:-
+
+first_name = "Sangamesh"
+last_name = "MK"
+numerical = "94"
+username = first_name + "_" + last_name + "." + numerical
+
+print(username)
+#O/P: Sangamesh_MK.94
+
+
+#Ex:02 Username Generation:-
+
+first_name = "matured"
+last_name = "mind"
+numerical = "3421"
+username = first_name + "_" + last_name + numerical
+
+print(username)
+#O/P: matured_mind3421
+
+
+#Ex: 03 Email Generation:-
+
+name = "sangameshmk"
+numerical = "94"
+domain = "gmail.com"
+Email = name + numerical + "@" + domain
+
+print(Email)
+#O/P: sangameshmk94@gmail.com
+
+#Ex:04 Creating file paths:-
+
+folder = "document"
+file = "report"
+format = "pdf"
+file_path = folder + "/" + file + "." + format
+
+print(file_path)
+#O/P: document/report.pdf
+
+
+#Ex:05 date format string:-
+
+day = "08"
+month = "08"
+year = "2006"
+date = day + "/" + month + "/" + year
+
+print(date)
+#O/P: 08/08/2006
+
+
+
+
+   
+   
