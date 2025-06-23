6a66c395004a9f9272d5b6be49f98e0aa7ee0e22
Sangamesh MK <sangameshmkuri94@gmail.com>
2025-06-23 13:18:36 +0530

    Update 01_Test.java

--- Java/01_Hello_world.java
+++ Java/01_Hello_world.java
@@ -1,1 +1,37 @@
+//01.
 
+public class Test {
+    
+    public static void main(String args[]) {
+        
+        System.out.println("HELLO, JAVA");
+    }
+}
+
+//O/P: HELLO, JAVA
+
+
+//02.
+
+public class Test {
+    
+    public static void main(String args[]) {
+    
+    System.out.println("HELLO, WOLRD!") ;
+    
+    }
+}
+
+//O/P: HELLO, WORLD!
+    
+//03.
+
+public class Test {
+    
+    public static void main(String args[]) {
+    
+    System.out.println("HII I'M SANGAMESH😊") ;
+    }
+}
+
+//O/P: HII I'M SANGAMESH😊
