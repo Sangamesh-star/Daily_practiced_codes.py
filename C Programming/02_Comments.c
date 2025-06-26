3c70138b153787f0bc278bc262a9e35685df42af
Sangamesh MK <sangameshmkuri94@gmail.com>
2025-06-26 20:25:05 +0530

    Create 02_Comments.c

---
+++ C Programming/02_Comments.c
@@ -0,0 +1,23 @@
+//Comments in Java
+
+//01. Single line comment :
+
+//This is an example for the single line comment.
+
+//02. Multi line comment:
+
+/* Java is one of the most popular 
+language, It was introduced in 1995 by
+ Sun Microsystems.Now owned by Oracle
+  co-operations */
+
+// NOTE:- We can also use single line comment symbol for the multi line comments 
+
+// Hi my name is Sangamesh MK.
+// I am studying CSE.
+// Now I am learning Many programming languages like python,java, many more.
+
+
+//NOTE:- We can also use multiline comment symbol for the Single line comments.
+
+/* I want to be the best developer😊.*/
