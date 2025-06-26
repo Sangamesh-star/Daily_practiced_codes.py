741942fca18df3d650f95aa8af2c8b6143e182bf
Sangamesh MK <sangameshmkuri94@gmail.com>
2025-06-26 17:51:46 +0530

    Create 02_assigning_variables.js

---
+++ Java Script/02_assigning_variables.js
@@ -0,0 +1,22 @@
+//In JavaScript we can assign values to the variables in three ways:-
+
+//Type One:
+//01.
+const sky_colour = "BLUE😊";
+console.log(sky_colour);
+//O/P: BLUE😊
+
+
+//Type Two:
+//01.
+var name = "I'M SANGAMESH MK😊";
+console.log(name);
+//O/P: I'M SANGAMESH MK😊
+
+
+//Type Three:
+//01.
+let dress_code = "Half shirt with normal pant😊";
+console.log(dress_code);
+//O/P: Half shirt with normal pant😊
+
