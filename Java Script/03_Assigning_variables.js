571fbc082cd09f6c298e4d1eef0137f09e74b86e
Sangamesh MK <sangameshmkuri94@gmail.com>
2025-06-27 16:01:29 +0530

    Update 03_Assigning_variables.js

--- Java Script/03_Assigning_variables.js
+++ Java Script/03_Assigning_variables.js
@@ -10,12 +10,19 @@ newMessage = "YOUR DAILY DATA IS EXHAUSTED😔!!!"
 console.log(newMessage);
 //O/P: YOUR DAILY DATA IS EXHAUSTED😔!!!
 
+console.log("GOOD NIGHT❤️");
+//O/P: GOOD NIGHT❤️
+
 
 //Type One:
 //01.
 const sky_colour = "BLUE😊";
 console.log(sky_colour);
 //O/P: BLUE😊
+
+const Night = "Its very Dark🌑";
+console.log(Night);
+//O/P: Its very Dark🌑
 
 
 //Type Two:
@@ -24,6 +31,10 @@ var name = "I'M SANGAMESH MK😊";
 console.log(name);
 //O/P: I'M SANGAMESH MK😊
 
+var message = "Sweet Dreams😴";
+console.log(message);
+//O/P: Sweet Dreams😴
+
 
 //Type Three:
 //01.
@@ -31,3 +42,6 @@ let dress_code = "Half shirt with normal pant😊";
 console.log(dress_code);
 //O/P: Half shirt with normal pant😊
 
+let text = "Take care❤️";
+console.log(text);
+//O/P: Take care❤️
